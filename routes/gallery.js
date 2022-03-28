ad8d95610cfaa5d84f9d1a6f294a47a9acac1623
root <habdig7oficial>
2022-03-28 11:57:27 -0300

    auto deploy

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,6 +1,11 @@
-
 module.exports = function (app) {
     app.get("/gallery",function (req,res) {
-        res.render("gallery.ejs")
+        res.render("gallery.ejs",{dados:""})
+    })
+
+    let multer = require("../config/multer")
+
+    app.post("/gallery",multer.single("imagem"),function (req,res) {
+        res.redirect("/gallery")
     })
 }
