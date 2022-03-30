d213e4a1d50bbbfc961b482b962721b3d2e16ae9
habdig7oficial <65318468+habdig7oficial@users.noreply.github.com>
2022-03-30 11:45:59 -0300

    Add files via upload

--- config/multer.js
+++ config/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const armazenamento = multer.diskStorage({
     destination:function (req,file,cb){
-        cb(null,"./assets/images")
+        cb(null,"./assets/images/uploads")
     },
 
     filename:function (req,file,cb){
@@ -11,7 +11,16 @@ const armazenamento = multer.diskStorage({
 })
 
 
-let upload = multer ({storage:armazenamento})
+let upload = multer ({storage:armazenamento,limits:{fileSize:1000},
+fileFilter:(req,file,cb)=>{
+    if (file.mimetype == "image/png" ||
+    file.mimetype == "image/svg" ||
+    file.mimetype == "image/jpg" ||
+    file.mimetype == "image/jpeg"
+
+) {
+    cb(null,true)
+}else{cb(null,true) /*return; cb(new Error("tipo inválido")*/}}})
 
 
 module.exports = upload
--- routes/gallery.js
+++ routes/gallery.js
@@ -2,13 +2,14 @@ module.exports = function (app) {
 
     let conexao = require("../config/database")
 
-    conexao()
+    conexao.conexao()
 
     let gallery = require("../models/gallery") 
 
     app.get("/gallery",async function (req,res) {
         let documento = await gallery.find()
         res.render("gallery.ejs",{dados:documento})
+        console.log(documento)
     })
 
     let multer = require("../config/multer")
